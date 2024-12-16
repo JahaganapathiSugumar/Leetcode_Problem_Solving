5428efd3ee654316a1ffc505e7f8dfbe26d724e4
JAHAGANAPATHI SUGUMAR <125806337+JahaganapathiSugumar@users.noreply.github.com>
2024-12-16 08:40:23 +0530

    Time: 67 ms (5.92%), Space: 16.3 MB (100%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.py
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.py
@@ -1,3 +1,10 @@
 class Solution:
     def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+
+        for num in range(k):
+            i = nums.index(min(nums))
+            nums[i] = nums[i] * multiplier
+
+        return nums
+
         
