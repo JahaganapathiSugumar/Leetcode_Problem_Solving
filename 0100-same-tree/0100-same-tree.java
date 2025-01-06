b07bbb132605eeaa7f499a7b25aa8d1583d7e725
JAHAGANAPATHI SUGUMAR <125806337+JahaganapathiSugumar@users.noreply.github.com>
2025-01-06 13:47:02 +0530

    Time: 0 ms (100%), Space: 41 MB (74.7%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -20,10 +20,14 @@ class Solution {
             return true;
         }
 
-        if(p != null && q != null && p.val == q.val){
-            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right) ;
+        if(p == null || q == null){
+            return false;
         }
 
-        return false;
+        if(p.val != q.val){
+            return false;
+        }
+
+         return isSameTree(p.left,q.left) && isSameTree(p.right,q.right) ;
     }
 }
