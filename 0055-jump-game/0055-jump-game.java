141349c1dfeca9866e99491cb05286998581f5d9
JAHAGANAPATHI SUGUMAR <125806337+JahaganapathiSugumar@users.noreply.github.com>
2025-02-28 23:39:30 +0530

    Time: 1 ms (99.91%), Space: 45.4 MB (91.87%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int final_pos = nums.length - 1;
+
+        for(int idx = nums.length - 2 ;idx >= 0;idx--){
+
+            if(idx + nums[idx] >= final_pos){
+                final_pos = idx;
+            }
+        }
+
+        return final_pos == 0;
+
+    }
+}
