7b4773db3bc0074298b48e18273c2ac604b17e30
JAHAGANAPATHI SUGUMAR <125806337+JahaganapathiSugumar@users.noreply.github.com>
2025-03-08 22:46:47 +0530

    Time: 2 ms (21.93%), Space: 41.4 MB (86.78%) - LeetHub

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -4,8 +4,8 @@ class Solution {
         int minoper = Integer.MAX_VALUE;
 
         for(int i = 0;i <= n - k;i++){
+            
             int whitecount = 0;
-
 
             for(int j = i;j < i + k;j++){
                 if(blocks.charAt(j) == 'W'){
